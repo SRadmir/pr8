03c10a45a70b205fa286df05f4b69a44ecbad62b
Radmir Sharafutdinov <rsarafutdinov186@icloud.com>
2024-11-03 14:40:21 +0500

    Модифицирован код программы задания 4, а именно добавлено проверка на число

--- homework4/12.py
+++ homework4/12.py
@@ -1,13 +1,28 @@
-a = float(input("Введите число а:"))
-b = float(input("Введите число b:"))
-l=[]
-if a<b:
-        for i in range(int(a)-1,int(b)+1):
-                if a<i<b:
-                        print(i)
-elif a>b:
-        for i in range(int(b)-1, int(a)+1):
-                if b<i<a:
-                        print(i)
-else:
-        print("a и b равны")
+def isdigit(di):
+        return di.lstrip('-').replace('.', '', 1).isdigit()
+while True:
+        a = input("Введите число а:")
+        if a.lower() == 'stop' or  a.lower() == 'break':
+                break
+        if not isdigit(a):
+                print("Некорректный ввод ")
+                continue
+        a=float(a)
+        b= input("Введите число b:")
+
+        if not isdigit(b):
+                print("Некорректный ввод")
+                continue
+        b=float(b)
+        if a<b:
+                for i in range(int(a)-1,int(b)+1):
+                        if a<i<b:
+                                print(i)
+                                continue
+        elif a>b:
+                for i in range(int(b)-1, int(a)+1):
+                        if b<i<a:
+                                print(i)
+                                continue
+        else:
+                print("a и b равны")
